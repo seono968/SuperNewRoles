6a38353607d400f5d96a5ed50c9f40d89a3df5f5
ykundesu <4476tv@gmail.com>
2022-10-07 18:10:56 +0900

    修正

--- SuperNewRoles/CustomObject/Beacon.cs
+++ SuperNewRoles/CustomObject/Beacon.cs
@@ -10,49 +10,52 @@ namespace SuperNewRoles.CustomObject
     {
         public static List<Beacon> AllBeacons = new();
         public static Sprite[] beaconAnimationSprites = new Sprite[3];
+        public static int maxid;
+        public int id = 0;
 
         public static Sprite GetBeaconAnimationSprite(int index)
         {
             if (beaconAnimationSprites == null || beaconAnimationSprites.Length == 0) return null;
             index = Mathf.Clamp(index, 0, beaconAnimationSprites.Length - 1);
-            foreach (PlayerControl p in CachedPlayer.AllPlayers)
-            {
-                CustomAnimation Conjurer_Beacon_Animation = new()
-                {
-                    Sprites = CustomAnimation.GetSprites("SuperNewRoles.Resources.ConjurerAnimation.Conjurer_Beacon", 60)
-                };
-                Transform Conjurer_Beacon1 = GameObject.Instantiate(GameObject.Find($"Beacon{Conjurer.Count}").transform);
-                Conjurer_Beacon_Animation.Start(30, Conjurer_Beacon1);
-            }
             return beaconAnimationSprites[index];
         }
 
 
-        public static void StartAnimation(int ventId)
+        public static void StartAnimation(int Id)
         {
-            Beacon beacon = AllBeacons.FirstOrDefault((x) => x?.vent != null && x.vent.Id == ventId);
+            Beacon beacon = AllBeacons.FirstOrDefault((x) => x.id == Id);
             if (beacon == null) return;
 
-            HudManager.Instance.StartCoroutine(Effects.Lerp(0.6f, new Action<float>((p) =>
+            CustomAnimation Conjurer_Beacon_Animation = new()
             {
-                beacon.BeaconRenderer.sprite = GetBeaconAnimationSprite((int)(p * beaconAnimationSprites.Length));
-                beacon.BeaconRenderer.sprite = GetBeaconAnimationSprite(0);
-            })));
+                Sprites = CustomAnimation.GetSprites("SuperNewRoles.Resources.ConjurerAnimation.Conjurer_Beacon", 60)
+            };
+            Transform Conjurer_Beacon1 = GameObject.Instantiate(GameObject.Find($"Beacon{beacon.id}").transform);
+            Conjurer_Beacon_Animation.Start(30, Conjurer_Beacon1);
         }
 
         private readonly GameObject GameObject;
         public Vent vent;
         private readonly SpriteRenderer BeaconRenderer;
+        static Transform Animation;
 
         public Beacon(Vector2 p)
         {
             GameObject = new GameObject($"Beacon{Conjurer.Count}") { layer = 11 };
-            GameObject.AddSubmergedComponent(SubmergedCompatibility.Classes.ElevatorMover);
+            //GameObject.AddSubmergedComponent(SubmergedCompatibility.Classes.ElevatorMover);
             Vector3 position = new(p.x, p.y, p.y / 1000f + 0.01f);
             position += (Vector3)PlayerControl.LocalPlayer.Collider.offset; // Add collider offset that DoMove moves the player up at a valid position
                                                                             // Create the marker
+            id = maxid;
+            maxid++;
             GameObject.transform.position = position;
             BeaconRenderer = GameObject.AddComponent<SpriteRenderer>();
+            CustomAnimation Conjurer_Beacon_Animation = new()
+            {
+                Sprites = CustomAnimation.GetSprites("SuperNewRoles.Resources.ConjurerAnimation.Conjurer_Beacon", 60)
+            };
+            Animation = GameObject.Instantiate(GameObject.Find($"Beacon{id}").transform, this.GameObject.transform);
+            Conjurer_Beacon_Animation.Start(30, Animation);
             BeaconRenderer.sprite = GetBeaconAnimationSprite(0);
             // Only render the beacon for the conjurer
             var playerIsTrickster = PlayerControl.LocalPlayer;
@@ -64,12 +67,15 @@ namespace SuperNewRoles.CustomObject
 
         public static void ClearBeacons()
         {
-            int[] num = { -1, -2, -3 };
+            maxid = 0;
+            int[] num = { 1, 2, 3 };
             foreach (var n in num)
             {
-                Logger.Info($"Beacon{Conjurer.Count + n}をClearします", "ClearBeacons");
-                GameObject.Find($"Beacon{Conjurer.Count + n}")?.SetActive(false);
-                GameObject.Find($"Beacon{Conjurer.Count + n}(Clone)")?.SetActive(false);
+                Logger.Info($"Beacon{n}をClearします", "ClearBeacons");
+                if (GameObject.Find($"Beacon{n}") != null)
+                    GameObject.Destroy(GameObject.Find($"Beacon{n}"));
+                if (GameObject.Find($"Beacon{n}(Clone)") != null)
+                    GameObject.Destroy(GameObject.Find($"Beacon{n}(Clone)"));
             }
         }
     }
